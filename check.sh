73955f294891f2962f76850ca6a527783cb781ef
trumully <truman.mulholland@gmail.com>
2024-11-15 12:51:46 +1300

    Revert workflow

--- check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-uv sync --all-extras --dev
-uv run ruff check --config ./pyproject.toml --output-format github
-uv run pytest tests --asyncio-mode=strict -n logical
